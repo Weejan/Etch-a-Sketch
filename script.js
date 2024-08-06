7707aab05e607480c162bbacd74173121cb5f5b0
weejan <weezan49@gmail.com>
2024-08-06 23:55:17 +0545

    Add button to change the grid and separating grid

--- script.js
+++ script.js
@@ -1,21 +1,26 @@
+let container = document.querySelector("#container")
+
 function grid(){
     let gridSize = prompt("Input your grid size")
-
-    let container = document.querySelector("#container")
+    let boxSize = 500/ gridSize
     for(i = 0; i < gridSize; i++){
-        let row = document.createElement('div');
+        let column = document.createElement('div');
         for(j = 0; j < gridSize; j++){
-            let column = document.createElement('div');
-            let boxSize = 500/ gridSize
-            console.log(boxSize)
-            column.setAttribute("class" ,"sqr-box")
-            let box = document.querySelectorAll(".sqr-box")
-            column.style.height = `${boxSize}px`
-            column.style.width = `${boxSize}px`
-            row.appendChild(column);
+            let row = document.createElement('div');
+            row.setAttribute("class" ,"sqr-box")
+            boxSizing(row,boxSize);
+            column.appendChild(row);
         }
-        container.appendChild(row)
+        container.appendChild(column)
     }
+}
+
+function boxSizing(row,boxSize){
+    row.style.height = `${boxSize}px`
+    row.style.width = `${boxSize}px`
+}
+
+function hover(){
     let box = document.querySelectorAll(".sqr-box")
     box.forEach((item)=>{item.addEventListener('mouseover',(e)=>{
         item.style.backgroundColor = "black"
@@ -23,4 +28,15 @@ function grid(){
     })
 }
 
+function gridButton(){
+    let button = document.createElement('button');
+    button.textContent = "Change Grid";
+    button.addEventListener('click',()=>{
+        let div = document.querySelectorAll('div');
+        div.forEach((item)=>{item.remove();})
+        grid()})
+    document.body.appendChild(button)
+}
 grid()
+hover()
+gridButton()
