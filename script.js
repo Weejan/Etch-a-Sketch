c28b1b012c52d9ce683de388832f7333108e07bd
weejan <weezan49@gmail.com>
2024-08-06 23:14:58 +0545

    Create auto sizing grid

--- script.js
+++ script.js
@@ -1,17 +1,21 @@
 function grid(){
-    let container = document.querySelector("#container")
+    let gridSize = prompt("Input your grid size")
 
-    for(i = 0; i < 16; i++){
+    let container = document.querySelector("#container")
+    for(i = 0; i < gridSize; i++){
         let row = document.createElement('div');
-        for(j = 0; j < 16; j++){
+        for(j = 0; j < gridSize; j++){
             let column = document.createElement('div');
+            let boxSize = 500/ gridSize
+            console.log(boxSize)
             column.setAttribute("class" ,"sqr-box")
+            let box = document.querySelectorAll(".sqr-box")
+            column.style.height = `${boxSize}px`
+            column.style.width = `${boxSize}px`
             row.appendChild(column);
-        
         }
         container.appendChild(row)
     }
-
     let box = document.querySelectorAll(".sqr-box")
     box.forEach((item)=>{item.addEventListener('mouseover',(e)=>{
         item.style.backgroundColor = "black"
